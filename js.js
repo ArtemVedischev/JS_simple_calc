51d671cdcaa8970861f907cac6896070a977fc98
Artem Vedischev <artvedy@gmail.com>
2017-10-31 23:33:21 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,135 @@
+window.onload = function(){
+ 
+}
+var mathOperation;
+var number = '';
+var number1;
+var number2;
+var result=0;
+function clear() {
+	var inputNumber = document.getElementById("number");
+	inputNumber.value = "";
+}
+function reset(){
+	clear();
+	number =  number1 = number2 = mathOperation = '';
+	return number, number1, number2, mathOperation;
+	
+}
+//get number
+function getNumber(num) {
+	number=document.getElementById("number").value;
+	switch (num) 
+	{
+		case 1: {
+			number += '1';
+			break;
+		}
+		case 2: {
+			number += '2';
+			break;
+		}
+		case 3: 
+		{
+			number += '3';
+			break;
+		}
+		case 4: {
+			number += '4';
+			break;
+		}
+		case 5: {
+			number += '5';
+			break;
+		}
+		case 6: {
+			number += '6';
+			break;
+		}
+		case 7: {
+			number += '7';
+			break;
+		}
+		case 8: {
+			number += '8';
+			break;
+		}
+		case 9: {
+			number += '9';
+			break;
+		}
+		case 0: {
+			number += '0';
+			break;
+		}
+		case 'point' : {
+			number += '.';
+			break;
+		}	
+	}
+	document.getElementById("number").value = number;
+	return number;
+}
+//choice of operation
+function getOperation(oper) {
+	switch (oper)
+  {
+	case 1: {  //button +
+      mathOperation = '+';
+	  number1 = parseFloat(document.getElementById("number").value);
+	  clear();
+      break;
+    }
+    case 2: {  //button -
+      mathOperation = '-';
+	  number1 = parseFloat(document.getElementById("number").value);
+	  clear();
+      break;
+    }
+    case 3: {  //button *
+      mathOperation = '*';
+	  number1 = parseFloat(document.getElementById("number").value);
+	  clear();
+      break;
+    }
+    case 4: {  //button /
+      mathOperation = '/';
+	  number1 = parseFloat(document.getElementById("number").value);
+	  clear();
+      break;
+    }
+  }
+  number = '';
+  return mathOperation, number1, number;
+}
+//get result
+function showResult() {
+	number2 = parseFloat(document.getElementById("number").value);
+	switch(mathOperation) {
+		case '+': {
+			result = number1 + number2;
+			break;	
+		}
+		case '-': {
+			result = number1 - number2;
+			break;	
+		}
+		case '*': {
+			result = number1 * number2;
+			break;	
+		}
+		case '/': {
+			result = number1 / number2;
+			break;
+		}
+		default: {
+			alert('Enter the number');
+		}			
+	}
+	var res = result;
+	var printResult = number1+mathOperation+number2+'='+result;
+	document.getElementById("number").value = res;	//return result
+	document.getElementById("getResult").innerHTML = printResult;//return result
+	console.log(number1+mathOperation+number2+'='+result);	
+
+}
